1cac3eed9678a04346bb252e548ef5a293c0d70a
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-10-25 17:25:34 -0400

    working parallel example

---
+++ xgboost/11parallel.py
@@ -0,0 +1,72 @@
+"""
+how to get the most from your hardware when using the XGBoost model. 
+check that the multi-threading support in XGBoost is enabled on your system.
+increasing the number of threads affects the performance of training XGBoost models.
+configure XGBoost and Cross-Validation in Python for minimum running time.
+"""
+# Otto, tune number of threads
+
+import time
+
+from matplotlib import pyplot
+from pandas import read_csv
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBClassifier
+
+# load data
+data = read_csv('train.csv.zip')
+data = data.sample(10000)
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# evaluate the effect of the number of threads
+results = []
+num_threads = [1, 2, 3, 4]
+for n in num_threads:
+    start = time.time()
+    model = XGBClassifier(nthread=n)
+    model.fit(X, label_encoded_y)
+    elapsed = time.time() - start
+    print(n, elapsed)
+    results.append(elapsed)
+
+# plot results
+pyplot.plot(num_threads, results)
+pyplot.ylabel('Speed (seconds)')
+pyplot.xlabel('Number of Threads')
+pyplot.title('XGBoost Training Speed vs Number of Threads')
+pyplot.show()
+
+# Otto, parallel cross validation
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# prepare cross validation
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+# Single Thread XGBoost, Parallel Thread CV
+start = time.time()
+model = XGBClassifier(nthread=1)
+results = cross_val_score(model, X, label_encoded_y, cv=kfold, scoring='neg_log_loss', n_jobs=-1)
+elapsed = time.time() - start
+print("Single Thread XGBoost, Parallel Thread CV: %f" % (elapsed))
+# Parallel Thread XGBoost, Single Thread CV
+start = time.time()
+model = XGBClassifier(nthread=-1)
+results = cross_val_score(model, X, label_encoded_y, cv=kfold, scoring='neg_log_loss', n_jobs=1)
+elapsed = time.time() - start
+print("Parallel Thread XGBoost, Single Thread CV: %f" % (elapsed))
+# Parallel Thread XGBoost and CV
+start = time.time()
+model = XGBClassifier(nthread=-1)
+results = cross_val_score(model, X, label_encoded_y, cv=kfold, scoring='neg_log_loss', n_jobs=-1)
+elapsed = time.time() - start
+print("Parallel Thread XGBoost and CV: %f" % (elapsed))
