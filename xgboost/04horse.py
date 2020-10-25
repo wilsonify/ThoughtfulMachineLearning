b052872cb037aedb8dfa40646f86cddd07277bca
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-10-25 12:05:07 -0400

    add imputer to horse example

--- xgboost/04horse.py
+++ xgboost/04horse.py
@@ -1,9 +1,11 @@
-# binary classification, missing data
+# binary classification, missing data, impute with mean
+import numpy
 from pandas import read_csv
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
 
 # load data
 dataframe = read_csv("data/horse-colic.csv")
@@ -11,10 +13,13 @@ dataset = dataframe.values
 # split data into X and y
 X = dataset[:, 0:27]
 Y = dataset[:, 27]
-# set missing values to 0
-X[X == ' ? '] = 0
+# set missing values to NaN
+X[X == ' ? '] = numpy.nan
 # convert to numeric
 X = X.astype('float32')
+# impute missing values as the mean
+imputer = SimpleImputer()
+imputed_x = imputer.fit_transform(X)
 # encode Y class values as integers
 label_encoder = LabelEncoder()
 label_encoder = label_encoder.fit(Y)
@@ -22,7 +27,7 @@ label_encoded_y = label_encoder.transform(Y)
 # split data into train and test sets
 seed = 7
 test_size = 0.33
-X_train, X_test, y_train, y_test = train_test_split(X, label_encoded_y,
+X_train, X_test, y_train, y_test = train_test_split(imputed_x, label_encoded_y,
                                                     test_size=test_size, random_state=seed)
 # fit model on training data
 model = XGBClassifier()
