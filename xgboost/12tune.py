03badf057a37fa41559d03ed6d899aceb1cba03d
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-10-25 14:12:53 -0400

    working tuning examples

---
+++ xgboost/12tune.py
@@ -0,0 +1,301 @@
+"""
+how to configure gradient boosting learning problems.
+weighting the addition of new trees to a gradient boosting model is called shrinkage or the learning rate.
+There is a trade-off between the number of trees and shrinkage
+consider limiting tree size and good defaults for tree depth and the number of terminal nodes.
+
+Grid search strategies to jointly tune the number and depth of decision trees
+
+adding a learning rate is intended to slow down the adaptation of the model to the training data.
+evaluate a range of learning rate values on your machine learning problem.
+evaluate the relationship of varying both the number of trees and the learning rate on your problem.
+
+stochastic boosting, subsample your training data to improve the generalization of your model
+tune row subsampling with XGBoost in Python and scikit-learn.
+tune column subsampling with XGBoost both per-tree and per-split.
+"""
+
+# XGBoost on Otto dataset, Tune n_estimators
+
+import matplotlib
+import numpy
+from matplotlib import pyplot
+from pandas import read_csv
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBClassifier
+
+matplotlib.use('Agg')
+# load data
+data = read_csv('data/train.csv.zip')
+data = data.sample(10000)
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+n_estimators = range(50, 400, 50)
+param_grid = dict(n_estimators=n_estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+# plot
+pyplot.errorbar(n_estimators, means, yerr=stds)
+pyplot.title("XGBoost n_estimators vs Log Loss")
+pyplot.xlabel('n_estimators')
+pyplot.ylabel('Log Loss')
+pyplot.savefig('n_estimators.png')
+
+# XGBoost on Otto dataset, Tune max_depth
+
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+max_depth = range(1, 11, 2)
+print(max_depth)
+param_grid = dict(max_depth=max_depth)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold,
+                           verbose=1)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+# plot
+pyplot.errorbar(max_depth, means, yerr=stds)
+pyplot.title("XGBoost max_depth vs Log Loss")
+pyplot.xlabel('max_depth')
+pyplot.ylabel('Log Loss')
+pyplot.savefig('max_depth.png')
+
+# XGBoost on Otto dataset, Tune n_estimators and max_depth
+
+# load data
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+n_estimators = [50, 100, 150, 200]
+max_depth = [2, 4, 6, 8]
+print(max_depth)
+param_grid = dict(max_depth=max_depth, n_estimators=n_estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold,
+                           verbose=1)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+# plot results
+scores = numpy.array(means).reshape(len(max_depth), len(n_estimators))
+for i, value in enumerate(max_depth):
+    pyplot.plot(n_estimators, scores[i], label='depth:' + str(value))
+pyplot.legend()
+pyplot.xlabel('n_estimators')
+pyplot.ylabel('Log Loss')
+pyplot.savefig('n_estimators_vs_max_depth.png')
+
+# XGBoost on Otto dataset, Tune learning_rate
+
+# load data
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+learning_rate = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3]
+param_grid = dict(learning_rate=learning_rate)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+# plot
+pyplot.errorbar(learning_rate, means, yerr=stds)
+pyplot.title("XGBoost learning_rate vs Log Loss")
+pyplot.xlabel('learning_rate')
+pyplot.ylabel('Log Loss')
+pyplot.savefig('learning_rate.png')
+
+# XGBoost on Otto dataset, Tune learning_rate and n_estimators
+
+# load data
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+n_estimators = [100, 200, 300, 400, 500]
+learning_rate = [0.0001, 0.001, 0.01, 0.1]
+param_grid = dict(learning_rate=learning_rate, n_estimators=n_estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+# plot results
+scores = numpy.array(means).reshape(len(learning_rate), len(n_estimators))
+for i, value in enumerate(learning_rate):
+    pyplot.plot(n_estimators, scores[i], label='learning_rate:' + str(value))
+pyplot.legend()
+pyplot.xlabel('n_estimators')
+
+pyplot.ylabel('Log Loss')
+pyplot.savefig('n_estimators_vs_learning_rate.png')
+
+# Plot performance for learning_rate=0.1
+
+
+n_estimators = [100, 200, 300, 400, 500]
+loss = [-0.001239, -0.001153, -0.001152, -0.001153, -0.001153]
+pyplot.plot(n_estimators, loss)
+pyplot.xlabel('n_estimators')
+pyplot.ylabel('Log Loss')
+pyplot.title('XGBoost learning_rate=0.1 n_estimators vs Log Loss')
+pyplot.show()
+
+# XGBoost on Otto dataset, tune subsample
+
+# load data
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+subsample = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0]
+param_grid = dict(subsample=subsample)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+# plot
+pyplot.errorbar(subsample, means, yerr=stds)
+pyplot.title("XGBoost subsample vs Log Loss")
+pyplot.xlabel('subsample')
+pyplot.ylabel('Log Loss')
+pyplot.savefig('subsample.png')
+
+# XGBoost on Otto dataset, tune colsample_bytree
+
+# load data
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+colsample_bytree = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0]
+param_grid = dict(colsample_bytree=colsample_bytree)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+# plot
+pyplot.errorbar(colsample_bytree, means, yerr=stds)
+pyplot.title("XGBoost colsample_bytree vs Log Loss")
+pyplot.xlabel('colsample_bytree')
+pyplot.ylabel('Log Loss')
+pyplot.savefig('colsample_bytree.png')
+
+# XGBoost on Otto dataset, tune colsample_bylevel
+
+matplotlib.use('Agg')
+
+# load data
+
+dataset = data.values
+# split data into X and y
+X = dataset[:, 0:94]
+
+y = dataset[:, 94]
+# encode string class values as integers
+label_encoded_y = LabelEncoder().fit_transform(y)
+# grid search
+model = XGBClassifier()
+colsample_bylevel = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0]
+param_grid = dict(colsample_bylevel=colsample_bylevel)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X, label_encoded_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+# plot
+pyplot.errorbar(colsample_bylevel, means, yerr=stds)
+pyplot.title("XGBoost colsample_bylevel vs Log Loss")
+pyplot.xlabel('colsample_bylevel')
+pyplot.ylabel('Log Loss')
+pyplot.savefig('colsample_bylevel.png')
