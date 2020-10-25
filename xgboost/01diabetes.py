3d6567a62b2b6d8cb33865b0bba4f8b5a6d24c21
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-10-24 22:21:15 -0400

    basic xgb example

--- xgboost/01diabetes.py
+++ xgboost/01diabetes.py
@@ -1,4 +1,5 @@
 from numpy import loadtxt
+from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
