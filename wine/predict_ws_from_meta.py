8e314d841251b66c22379c64e13daaf9a9de6365
wilsonify <tom.andrew.wilson@gmail.com>
2023-06-29 07:25:54 -0400

    more models

--- wine/predict_ws_from_meta.py
+++ wine/predict_ws_from_meta.py
@@ -23,25 +23,38 @@ from pycaret.regression import (
 )
 from pycaret.utils import version
 
-id_cols = ['productID']
+id_cols = ['productID',"name","pProductID", "wine_url","pageName","uploadDate"]
 target_cols = ['WS']
+categorical_cols=[
+    "description", "productPrice", "productCompetitiveIntensity", "ProductAvailability", "priceCurrency", 	"additionalType" ,"productOrigin", "productVarietal", "productRegion"
+]
+extra_cols=["shippingRegion", "shipToState"]
+other_ratings = [ 'JS', 'WW', 'D', 'BH', 'W&S', 'WE', 'RP', 'JD', 'SJ', 'V', 'CG', 'TP' ]
 predictors = [
     'productStock',
     'price',
-    'averageRating_bestRating',
-    'averageRating_worstRating',
-    'bestRating',
-    'worstRating',
-    'prodAlcoholVolume_text',
-    'prodAlcoholPercent_percent',
-    'JS', 'WW', 'D', 'BH',
-    'W&S', 'WE', 'RP', 'JD',
-    'SJ', 'V', 'CG', 'TP'
+    'prodAlcoholPercent_percent'
+    #'averageRating_bestRating',
+    #'averageRating_worstRating',
+    #'bestRating',
+    #'worstRating',
+    #'prodAlcoholVolume_text',
 ]
 
 print(f"pycaret version = {version()}")
 print("1. Loading Dataset")
 df = pd.read_csv("wine_spectator.csv")
+df = df.drop_duplicates('wine_url') 
+
+# +
+
+unnamed_cols=df.filter(regex="Unnamed")
+df = df.drop(categorical_cols, axis=1)
+df = df.drop(extra_cols, axis=1)
+df = df.drop(unnamed_cols, axis=1)
+df = df.drop(other_ratings, axis=1)
+
+df = df[id_cols+predictors+target_cols]
 indices = list(df.index)
 n_samples = len(indices)
 train_samples = int(0.8 * n_samples)
@@ -55,6 +68,7 @@ df_train = df.loc[pick_k_random_indices, :]
 df_train_id = df_train[id_cols]
 df_train = df_train.drop(id_cols, axis=1)
 df_train = df_train.dropna(subset=target_cols, axis=0)
+# -
 
 df_test = df.loc[not_picked_indices, :]
 df_test_id = df_test[id_cols]
@@ -69,22 +83,32 @@ reg1 = setup(
     data=df_train,
     target="WS",
     experiment_name='ws_from_meta',
-    imputation_type='iterative'
+    imputation_type='iterative',    
 )
 
+# +
 print("3. Compare Baseline")
 
 best_model = compare_models(fold=5)
+# -
 
 print("4. Create Model")
-lightgbm = create_model('lightgbm')
-lgbms = [create_model('lightgbm', learning_rate=i) for i in np.arange(0.1, 1, 0.1)]
-print(len(lgbms))
+lr = create_model('lr')
+lr = tune_model(lr, n_iter=5, optimize='RMSE')
+evaluate_model(lr)
+
+rf = create_model('rf')
+rf = tune_model(rf, n_iter=5, optimize='RMSE')
+evaluate_model(rf)
+
+knn=create_model('knn')
+knn = tune_model(knn, n_iter=5, optimize='RMSE')
+evaluate_model(knn)
 
 print("5. Tune Hyperparameters")
-tuned_lightgbm = tune_model(lightgbm, n_iter=50, optimize='MAE')
-
-print(tuned_lightgbm)
+lightgbm = create_model('lightgbm')
+lightgbm = tune_model(lightgbm, n_iter=5, optimize='MAE')
+evaluate_model(lightgbm)
 
 print("6. Ensemble Model")
 
@@ -95,6 +119,8 @@ bagged_dt = ensemble_model(dt, n_estimators=50)
 boosted_dt = ensemble_model(dt, method='Boosting')
 
 print("7. Blend Models")
+
+list(models().index)
 
 top_five = compare_models(n_select=5, fold=5, include=list(models().index))
 
@@ -135,13 +161,17 @@ pred_holdouts.head()
 predict_new = predict_model(best, data=df_test)
 predict_new.head()
 
+from matplotlib import pyplot as plt
+plt.scatter(predict_new['prediction_label'],df_test_truth)
+
 save_model(best, model_name='best-model')
 
 loaded_bestmodel = load_model('best-model')
 print(loaded_bestmodel)
 
-set_config(display='diagram')
 print(loaded_bestmodel[0])
 
 X_train = get_config('X_train')
 X_train.head()
+
+
