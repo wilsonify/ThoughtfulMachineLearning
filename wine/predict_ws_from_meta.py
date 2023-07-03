e285d91c6f9e84a80640113717ee2d3935a4c33a
wilsonify <tom.andrew.wilson@gmail.com>
2023-07-03 08:39:26 -0400

    update best-model

--- wine/predict_ws_from_meta.py
+++ wine/predict_ws_from_meta.py
@@ -1,9 +1,8 @@
 import random
 
-import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from pycaret.regression import get_config
-from pycaret.regression import set_config
 from pycaret.regression import (
     setup,
     compare_models,
@@ -13,8 +12,6 @@ from pycaret.regression import (
     models,
     blend_models,
     stack_models,
-    plot_model,
-    evaluate_model,
     interpret_model,
     automl,
     predict_model,
@@ -27,38 +24,39 @@ from pycaret.regression import (
 from pycaret.utils import version
 from yellowbrick.regressor import CooksDistance
 
-id_cols = ['productID',"name","pProductID", "wine_url","pageName","uploadDate"]
+id_cols = ['productID', "name", "pProductID", "wine_url", "pageName", "uploadDate"]
 target_cols = ['WS']
-categorical_cols=[
-    "description", "productPrice", "productCompetitiveIntensity", "ProductAvailability", "priceCurrency", 	"additionalType" ,"productOrigin", "productVarietal", "productRegion"
+categorical_cols = [
+    "description", "productPrice", "productCompetitiveIntensity", "ProductAvailability", "priceCurrency",
+    "additionalType", "productOrigin", "productVarietal", "productRegion"
 ]
-extra_cols=["shippingRegion", "shipToState"]
-other_ratings = [ 'JS', 'WW', 'D', 'BH', 'W&S', 'WE', 'RP', 'JD', 'SJ', 'V', 'CG', 'TP' ]
+extra_cols = ["shippingRegion", "shipToState"]
+other_ratings = ['JS', 'WW', 'D', 'BH', 'W&S', 'WE', 'RP', 'JD', 'SJ', 'V', 'CG', 'TP']
 predictors = [
     'productStock',
     'price',
     'prodAlcoholPercent_percent'
-    #'averageRating_bestRating',
-    #'averageRating_worstRating',
-    #'bestRating',
-    #'worstRating',
-    #'prodAlcoholVolume_text',
+    # 'averageRating_bestRating',
+    # 'averageRating_worstRating',
+    # 'bestRating',
+    # 'worstRating',
+    # 'prodAlcoholVolume_text',
 ]
 
 print(f"pycaret version = {version()}")
 print("1. Loading Dataset")
 df = pd.read_csv("wine_spectator.csv")
-df = df.drop_duplicates('wine_url') 
+df = df.drop_duplicates('wine_url')
 
 # +
 
-unnamed_cols=df.filter(regex="Unnamed")
+unnamed_cols = df.filter(regex="Unnamed")
 df = df.drop(categorical_cols, axis=1)
 df = df.drop(extra_cols, axis=1)
 df = df.drop(unnamed_cols, axis=1)
 df = df.drop(other_ratings, axis=1)
 
-df = df[id_cols+predictors+target_cols]
+df = df[id_cols + predictors + target_cols]
 indices = list(df.index)
 n_samples = len(indices)
 train_samples = int(0.8 * n_samples)
@@ -84,14 +82,14 @@ print(list(df.describe()))
 
 visualizer = CooksDistance()
 visualizer.fit(df_train[predictors], df_train["WS"])
-#visualizer.show()
+# visualizer.show()
 
 df_train['cooks'] = visualizer.distance_
-high_influence = df_train['cooks']>1.0
-high_price = df_train['price']>500
+high_influence = df_train['cooks'] > 1.0
+high_price = df_train['price'] > 500
 
 df_train = df_train[~high_influence & ~high_price]
-df_train = df_train.drop('cooks',axis=1)
+df_train = df_train.drop('cooks', axis=1)
 
 df_train_id = df_train_id[~high_influence & ~high_price]
 
@@ -100,7 +98,7 @@ reg1 = setup(
     data=df_train,
     target="WS",
     experiment_name='ws_from_meta',
-    imputation_type='iterative',    
+    # imputation_type='iterative',
 )
 
 # +
@@ -112,20 +110,20 @@ best_model = compare_models(fold=5)
 print("4. Create Model")
 lr = create_model('lr')
 lr = tune_model(lr, n_iter=5, optimize='RMSE')
-evaluate_model(lr)
+# evaluate_model(lr)
 
 rf = create_model('rf')
 rf = tune_model(rf, n_iter=5, optimize='RMSE')
-evaluate_model(rf)
+# evaluate_model(rf)
 
-knn=create_model('knn')
+knn = create_model('knn')
 knn = tune_model(knn, n_iter=5, optimize='RMSE')
-evaluate_model(knn)
+# evaluate_model(knn)
 
 print("5. Tune Hyperparameters")
 lightgbm = create_model('lightgbm')
 lightgbm = tune_model(lightgbm, n_iter=5, optimize='MAE')
-evaluate_model(lightgbm)
+# evaluate_model(lightgbm)
 
 print("6. Ensemble Model")
 
@@ -149,13 +147,10 @@ stacker = stack_models(estimator_list=top_five)
 
 print("9. Analyze Model")
 
-plot_model(dt)
-
-plot_model(dt, plot='error')
-
-plot_model(dt, plot='feature')
-
-evaluate_model(dt)
+# plot_model(dt)
+# plot_model(dt, plot='error')
+# plot_model(dt, plot='feature')
+# evaluate_model(dt)
 
 print("10. Interpret Model")
 
@@ -178,16 +173,13 @@ pred_holdouts.head()
 predict_new = predict_model(best, data=df_test)
 predict_new.head()
 
-from matplotlib import pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot()
-ax.scatter(predict_new['prediction_label'],df_test_truth)
+ax.scatter(predict_new['prediction_label'], df_test_truth)
 ax.set_xlabel('predicted WS rating')
 ax.set_ylabel('actual WS rating')
 
 save_model(best, model_name='best-model')
-
-convert_model()
 
 loaded_bestmodel = load_model('best-model')
 print(loaded_bestmodel)
@@ -197,10 +189,22 @@ print(loaded_bestmodel[0])
 X_train = get_config('X_train')
 X_train.head()
 
-convert_model(best_model,'c')
+#convert_model(best_model, 'c')
+#convert_model(best_model, 'python')
+#convert_model(best_model, 'java')
+#convert_model(best_model, 'javascript')
+#convert_model(best_model, 'c')
+#convert_model(best_model, 'c#')
+#convert_model(best_model, 'f#')
+#convert_model(best_model, 'go')
+#convert_model(best_model, 'haskell')
+#convert_model(best_model, 'php')
+#convert_model(best_model, 'powershell')
+#convert_model(best_model, 'r')
+#convert_model(best_model, 'ruby')
+#convert_model(best_model, 'vb')
+#convert_model(best_model, 'dart')
 
-create_api(best_model,api_name='predict_ws_from_meta')
+create_api(best_model, api_name='predict_ws_from_meta')
 
 create_docker(api_name='predict_ws_from_meta')
-
-
