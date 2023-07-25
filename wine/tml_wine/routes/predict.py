194a8a9e2900318576b1041f8c2b14a7484d1843
wilsonify <tom.a.wilson@gmail.com>
2023-07-25 07:49:19 -0400

    consolidate predict

--- wine/tml_wine/routes/predict.py
+++ wine/tml_wine/routes/predict.py
@@ -1,9 +1,27 @@
 import pandas as pd
+from pydantic import BaseModel, Field
+from pydantic import create_model
 
 from tml_wine.main import app
 from tml_wine.models.models import virtual_ws_meta, virtual_ws_other, virtual_ws_ensemble
-from tml_wine.predict_ws_api import input_model, output_model
 from tml_wine.predict_ws_inference import predictors, other_ratings, model_columns, pdesire, wdesire, composite
+
+dir(BaseModel)
+dir(Field)
+
+# Create input/output pydantic models
+input_model = create_model("predict_ws_input", **{
+    'productStock': 22.0,
+    'price': 79.98999786376953,
+    'prodAlcoholPercent_percent': 14.5,
+    'JS': 93.0, 'WW': 93.0, 'D': 93.0, 'BH': 93.0,
+    'W&S': 93.0, 'WE': 93.0, 'RP': 92.0, 'JD': 93.0, 'SJ': 93.0,
+    'V': 93.0, 'CG': 93.0, 'TP': 93.0
+})
+output_model = create_model("predict_ws_output", **{
+    "ws_pred": 90.0,
+    "composite_desire": 100.0
+})
 
 
 @app.post("/predict", response_model=output_model)
