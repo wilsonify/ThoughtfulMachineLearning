d503942cad8e2722cfbcfb08b26ab27542bc443e
wilsonify <tom.a.wilson@gmail.com>
2023-08-01 02:19:18 -0400

    decompose openapi.yaml

--- wine/openapi/tml_wine_generated/openapi_server/__main__.py
+++ wine/openapi/tml_wine_generated/openapi_server/__main__.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
+import logging
+import os
 
 import connexion
+from json_ref_dict import RefDict, materialize
 
 from openapi_server import encoder
 
+path_to_here = os.path.abspath(os.path.dirname(__file__))
+
+logging_config_dict = dict(
+    version=1,
+    formatters={
+        "simple": {
+            "format": """%(asctime)s | %(filename)s | %(lineno)d | %(levelname)s | %(message)s"""
+        }
+    },
+    handlers={"console": {"class": "logging.StreamHandler", "formatter": "simple"}},
+    root={"handlers": ["console"], "level": logging.DEBUG},
+)
+
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
+    openapi_refdict = RefDict(f"{path_to_here}/openapi/openapi.yaml")
+    openapi_dict = materialize(openapi_refdict)
+    app = connexion.App(__name__)
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Wine Observer'},
-                pythonic_params=True)
-
+    app.add_api(
+        specification=openapi_dict,
+        arguments={'title': 'Wine Observer'},
+        pythonic_params=True
+    )
     app.run(port=8080)
 
 
