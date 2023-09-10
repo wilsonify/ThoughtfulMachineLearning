48856d26c48296aee646ec9b29132416d7d7e72b
wilsonify <tom.a.wilson@gmail.com>
2023-09-10 16:00:16 -0400

    update steps

--- wine/steps/infra/iam/.terragrunt-cache/GfJ-gxURhsy-QwPqkNLprpD8Hng/TedR7e2hjseOPmRmdfa-pFZc8Z4/README.md
+++
@@ -1,2 +0,0 @@
-# serverless-architectures-aws
-The code repository for the Serverless Architectures on AWS book
--- wine/steps/infra/lambda/lambda.tf
+++ wine/steps/infra/lambda/lambda.tf
@@ -3,30 +3,103 @@ variable "aws_account_number" {
   type        = string
   default     = "064592191516"
 }
-resource "aws_lambda_function" "twentyfour-hour-video-python-transcode-video" {
-  function_name                  = "twentyfour-hour-video-python-transcode-video"
+
+resource "aws_lambda_function" "lambda-wine-s01" {
+  function_name                  = "wine-s01-scrape"
+  architectures                  = ["x86_64"]
+  image_uri                      = "${var.aws_account_number}.dkr.ecr.us-east-1.amazonaws.com/wine-s01-scrape:latest"
+  memory_size                    = "1024"
+  package_type                   = "Image"
+  reserved_concurrent_executions = "-1"
+  role                           = "arn:aws:iam::${var.aws_account_number}:role/iam-role-wine-lambda"
+  timeout                        = "6"
+  image_config { command = ["s01_wine_scrape/__main__.lambda_handler"] }
+  tracing_config { mode = "PassThrough" }
+  environment {
+    variables = {
+      INPUT_BUCKET  = "${var.aws_account_number}-wine-input"
+      OUTPUT_BUCKET = "${var.aws_account_number}-wine-output"
+    }
+  }
+}
+
+resource "aws_lambda_function" "lambda-wine-s02" {
+  function_name                  = "wine-s02-create-dataset"
   architectures                  = ["x86_64"]
   image_uri                      = "${var.aws_account_number}.dkr.ecr.us-east-1.amazonaws.com/transcode_video_python:latest"
   memory_size                    = "1024"
   package_type                   = "Image"
   reserved_concurrent_executions = "-1"
   role                           = "arn:aws:iam::${var.aws_account_number}:role/transcode-video"
-  source_code_hash               = "678ee8b39e57380f9051d0e6ed2ee31c1c4981f7e123639ca3e2210ba54fa8db"
+  timeout                        = "6"
+  image_config { command = ["transcode_video_python/__main__.lambda_handler"] }
+  tracing_config { mode = "PassThrough" }
   environment {
     variables = {
-      MEDIA_ENDPOINT          = "https://lxlxpswfb.mediaconvert.us-east-1.amazonaws.com"
-      MEDIA_ROLE              = "arn:aws:iam::${var.aws_account_number}:role/media-convert-role"
-      TRANSCODED_VIDEO_BUCKET = "${var.aws_account_number}-serverless-video-transcode-python"
+      INPUT_BUCKET  = "${var.aws_account_number}-serverless-video-transcode-python"
+      OUTPUT_BUCKET = "${var.aws_account_number}-serverless-video-transcode-python"
     }
   }
-  image_config { command = ["transcode_video_python/__main__.lambda_handler"] }
-  tags = {
-    STAGE = "dev"
-  }
-  timeout = "6"
-  tracing_config {
-    mode = "PassThrough"
-  }
-
 }
+
+resource "aws_lambda_function" "lambda-wine-s03" {
+  function_name                  = "wine-s03-fit"
+  architectures                  = ["x86_64"]
+  image_uri                      = "${var.aws_account_number}.dkr.ecr.us-east-1.amazonaws.com/transcode_video_python:latest"
+  memory_size                    = "1024"
+  package_type                   = "Image"
+  reserved_concurrent_executions = "-1"
+  role                           = "arn:aws:iam::${var.aws_account_number}:role/transcode-video"
+  timeout                        = "6"
+  image_config { command = ["transcode_video_python/__main__.lambda_handler"] }
+  tracing_config { mode = "PassThrough" }
+  environment {
+    variables = {
+      INPUT_BUCKET  = "${var.aws_account_number}-serverless-video-transcode-python"
+      OUTPUT_BUCKET = "${var.aws_account_number}-serverless-video-transcode-python"
+    }
+  }
+}
+
+resource "aws_lambda_function" "lambda-wine-s04" {
+  function_name                  = "wine-s04-predict"
+  architectures                  = ["x86_64"]
+  image_uri                      = "${var.aws_account_number}.dkr.ecr.us-east-1.amazonaws.com/transcode_video_python:latest"
+  memory_size                    = "1024"
+  package_type                   = "Image"
+  reserved_concurrent_executions = "-1"
+  role                           = "arn:aws:iam::${var.aws_account_number}:role/transcode-video"
+  timeout                        = "6"
+  image_config { command = ["transcode_video_python/__main__.lambda_handler"] }
+  tracing_config { mode = "PassThrough" }
+  environment {
+    variables = {
+      INPUT_BUCKET  = "${var.aws_account_number}-serverless-video-transcode-python"
+      OUTPUT_BUCKET = "${var.aws_account_number}-serverless-video-transcode-python"
+    }
+  }
+}
+
+resource "aws_lambda_function" "lambda-wine-s05" {
+  function_name                  = "wine-s05-score"
+  architectures                  = ["x86_64"]
+  image_uri                      = "${var.aws_account_number}.dkr.ecr.us-east-1.amazonaws.com/transcode_video_python:latest"
+  memory_size                    = "1024"
+  package_type                   = "Image"
+  reserved_concurrent_executions = "-1"
+  role                           = "arn:aws:iam::${var.aws_account_number}:role/transcode-video"
+  timeout                        = "6"
+  image_config { command = ["transcode_video_python/__main__.lambda_handler"] }
+  tracing_config { mode = "PassThrough" }
+  environment {
+    variables = {
+      INPUT_BUCKET  = "${var.aws_account_number}-serverless-video-transcode-python"
+      OUTPUT_BUCKET = "${var.aws_account_number}-serverless-video-transcode-python"
+    }
+  }
+}
+
+
+
+
 
