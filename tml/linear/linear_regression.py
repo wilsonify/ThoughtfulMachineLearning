a05834515a02361e2437bcee791d82f5717034a5
wilsonify <tom.andrew.wilson@gmail.com>
2024-02-10 17:48:06 -0500

    better plots

--- tml/linear/linear_regression.py
+++ tml/linear/linear_regression.py
@@ -1,9 +1,11 @@
 import logging
 from logging.config import dictConfig
 
+import matplotlib.pyplot as plt
 import numpy as np
 import statsmodels.api as sm
 from numpy.linalg import LinAlgError
+from scipy import stats
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
@@ -44,20 +46,14 @@ def get_list_to_matrix(x_list):
     return result
 
 
-def get_y_as_a_column():
-    y_nda = get_list_to_matrix(daily_minutes_good)
-    return y_nda
-
-
 def get_x_sans_intercept():
     num_friends_good_nda = get_list_to_matrix(num_friends_good)
     return num_friends_good_nda
 
 
-def get_x_with_an_intercept():
-    num_friends_good_nda = get_list_to_matrix(num_friends_good)
-    ones = np.ones(len(num_friends_good)).reshape(-1, 1)
-    X_with_intercept = np.hstack((ones, num_friends_good_nda))
+def get_x_with_an_intercept(x_nda):
+    ones = np.ones(len(x_nda)).reshape(-1, 1)
+    X_with_intercept = np.hstack((ones, x_nda))
     return X_with_intercept
 
 
@@ -67,6 +63,56 @@ def least_squares_fit(X, Y):
         beta_hat = np.linalg.inv(X.T @ X) @ X.T @ Y
     except LinAlgError:
         beta_hat = np.linalg.pinv(X.T @ X) @ X.T @ Y
+    return beta_hat
+
+
+def least_squares_fit_hh(X, Y):
+    x_nrows = X.shape[0]
+    x_mcols = X.shape[1]
+    y_nrows = Y.shape[0]
+    y_mcols = Y.shape[1]
+    assert x_nrows == y_nrows
+    degrees_of_freedom = x_nrows - x_mcols - y_mcols
+    confidence_level = 0.95
+    tail = 1 - confidence_level
+    right_tail = tail / 2
+    right_quantile = 1 - right_tail
+    t_crit = stats.t.ppf(right_quantile, degrees_of_freedom)
+
+    # Implement least squares fit using matrix operations
+    beta_hat = least_squares_fit(X, Y)
+    se = std_error(X, Y)
+
+    margin_of_error = t_crit * se[1]
+    beta_hat[1] += margin_of_error
+    x_bar = np.mean(X[1])
+    y_bar = np.mean(Y)
+    beta_hat[0] = y_bar - x_bar * beta_hat[1]
+    return beta_hat
+
+
+def least_squares_fit_ll(X, Y):
+    x_nrows = X.shape[0]
+    x_mcols = X.shape[1]
+    y_nrows = Y.shape[0]
+    y_mcols = Y.shape[1]
+    assert x_nrows == y_nrows
+    degrees_of_freedom = x_nrows - x_mcols - y_mcols
+    confidence_level = 0.95
+    tail = 1 - confidence_level
+    right_tail = tail / 2
+    right_quantile = 1 - right_tail
+    t_crit = stats.t.ppf(right_quantile, degrees_of_freedom)
+
+    # Implement least squares fit using matrix operations
+    beta_hat = least_squares_fit(X, Y)
+    se = std_error(X, Y)
+
+    margin_of_error = t_crit * se[1]
+    beta_hat[1] -= margin_of_error
+    x_bar = np.mean(X[1])
+    y_bar = np.mean(Y)
+    beta_hat[0] = y_bar - x_bar * beta_hat[1]
     return beta_hat
 
 
@@ -100,15 +146,48 @@ def std_error(X, y_true):
     return std_error_value
 
 
+def plot_with_line(ax, x, y):
+    x_intercept = get_x_with_an_intercept(x)
+    beta = least_squares_fit(x_intercept, y)
+    beta_hh = least_squares_fit_hh(x_intercept, y)
+    beta_ll = least_squares_fit_ll(x_intercept, y)
+    se = std_error(x_intercept, y)
+    # Plotting
+    ax.scatter(x, y)
+    ax.set_xlabel('Number of Friends')
+    ax.set_ylabel('Daily Minutes Spent on the Site')
+    ax.set_title('Relationship between Number of Friends and Daily Minutes Spent')
+    # Plot the regression line
+    y_pred = x_intercept @ beta
+    ax.plot(x, y_pred, color='red')
+    y_pred_hh = x_intercept @ beta_hh
+    ax.plot(x, y_pred_hh, color='orange')
+    y_pred_ll = x_intercept @ beta_ll
+    ax.plot(x, y_pred_ll, color='green')
+
+    ax.plot(np.mean(x), np.mean(x), color='black')
+    ax.plot(np.mean(y), np.mean(y), color='black')
+
+
 def main():
-    X_with_intercept = get_x_with_an_intercept()
-    Y = get_y_as_a_column()
+    num_friends_good_nda = get_list_to_matrix(num_friends_good)
+    X_with_intercept = get_x_with_an_intercept(num_friends_good_nda)
+
+    daily_minutes_good_nda = get_list_to_matrix(daily_minutes_good)
+    Y = daily_minutes_good_nda
 
     beta = least_squares_fit(X_with_intercept, Y)
+    y_pred = X_with_intercept @ beta
     logging.info("intercept: %r", beta[0])
     logging.info("slope: %r", beta[1])
-    # r_squared_f_vs_m = r_squared(alpha_, beta_, X[:, 0], Y)
-    # logging.info("r-squared: %r", r_squared_f_vs_m)
+
+    r_squared_f_vs_m = r_squared(Y, y_pred)
+    logging.info("r-squared: %r", r_squared_f_vs_m)
+
+    fig = plt.figure(figsize=(15, 15))
+    ax = fig.add_subplot(111)
+    plot_with_line(ax, num_friends_good_nda, daily_minutes_good_nda)
+    plt.show()
 
 
 def test_main():
@@ -123,10 +202,10 @@ def test_main():
 
 def test_least_squares_fit():
     num_friends_good_nda = get_list_to_matrix(num_friends_good)
-    daily_minutes_good_nda = get_list_to_matrix(daily_minutes_good)
+    X_with_intercept = get_x_with_an_intercept(num_friends_good_nda)
 
-    X_with_intercept = get_x_with_an_intercept()
-    Y = get_y_as_a_column()
+    daily_minutes_good_nda = get_list_to_matrix(daily_minutes_good)
+    Y = daily_minutes_good_nda
 
     # Least squares fit using matrix operations
     beta = least_squares_fit(X_with_intercept, Y)
@@ -165,11 +244,7 @@ def test_non_intertable():
     X = get_list_to_matrix([[1, 2, 3], [2, 4, 6], [3, 6, 9], ])
     Y = get_list_to_matrix([[1], [2], [3]])
 
-    # Calculate X.T @ X
-    X_T_X = X.T @ X
-
-    # Check if X.T @ X is invertible
-    is_invertible = np.linalg.det(X_T_X) != 0
+    is_invertible = np.linalg.det(X.T @ X) != 0
     assert not is_invertible
 
     beta = least_squares_fit(X, Y)
