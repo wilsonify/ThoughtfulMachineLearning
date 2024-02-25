30c9f157ac8e74302baf23972fe854ebfec383a3
wilsonify <tom.andrew.wilson@gmail.com>
2024-02-25 11:56:32 -0500

    main_scrape_wine_parallel

--- tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/issue_correction/save_missing_wines_to_html.py
+++ tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/issue_correction/save_missing_wines_to_html.py
@@ -9,6 +9,7 @@ from boto3 import Session
 
 from io_library.list_objects_s3 import list_objects_s3
 from s01_wine_scrape import OUTPUT_BUCKET, OUTPUT_PREFIX
+from s01_wine_scrape.pa_wines.s04_wine_html_to_json import main_scrape_one_wine
 
 
 def main_scrape_missing_wine_one_page(page_key):
@@ -22,8 +23,9 @@ def main_scrape_missing_wine_one_page(page_key):
     )
     csv_content = resp['Body'].read()
     missing_df = pd.read_csv(BytesIO(csv_content))
-    missing_df = missing_df[missing_df["detect_missing"]]
-    for row in missing_df.iterrows():
+    missing_html_df = missing_df[~missing_df["has_html"]]
+
+    for ind, row in missing_html_df.iterrows():
         count += 1
         time.sleep(5)
         wine_url = row["wine_url"]
@@ -36,6 +38,12 @@ def main_scrape_missing_wine_one_page(page_key):
             Bucket=OUTPUT_BUCKET,
             Key=wine_key
         )
+
+    missing_json_df = missing_df[~missing_df["has_json"]]
+    for ind, row in missing_json_df.iterrows():
+        suffix = row["suffix"]
+        wine_key = f"{OUTPUT_PREFIX}/{today_date_str}/html/wines/{suffix}.html"
+        main_scrape_one_wine(wine_key)
 
 
 def main_scrape_wine_parallel():
@@ -50,3 +58,8 @@ def main_scrape_wine_parallel():
         for obj in objs:
             print(obj)
             executor.submit(main_scrape_missing_wine_one_page, obj)
+
+
+if __name__ == "__main__":
+    # main_scrape_missing_wine_one_page("wine/s01-scrape/2024-02-25/csv/reports/page_01_missing.csv")
+    main_scrape_wine_parallel()
--- tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/issue_detection/detect_missing.py
+++ tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/issue_detection/detect_missing.py
@@ -3,7 +3,6 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
-import bs4
 import pandas as pd
 from boto3 import Session
 
@@ -11,6 +10,29 @@ from io_library.list_objects_s3 import list_objects_s3
 from io_library.read_from_s3 import read_dict_from_json_s3
 from io_library.write_to_s3 import write_csv_to_s3
 from s01_wine_scrape import OUTPUT_BUCKET, OUTPUT_PREFIX
+
+
+def check_for_json(count, wine_key_json):
+    try:
+        read_dict_from_json_s3(bucket=OUTPUT_BUCKET, key=wine_key_json)
+        count += 1
+        print(f"found wine_key_json = {wine_key_json}")
+        has_json = True
+    except:
+        print(f"missing wine_key_json = {wine_key_json}")
+        has_json = False
+    return has_json
+
+
+def check_for_html(wine_key_html):
+    try:
+        s3_session = Session()
+        s3_client = s3_session.client('s3')
+        s3_client.get_object(Bucket=OUTPUT_BUCKET, Key=wine_key_html)
+        has_html = True
+    except:
+        has_html = False
+    return has_html
 
 
 def main_detect_missing(page_key):
@@ -31,32 +53,22 @@ def main_detect_missing(page_key):
         link_text = wine["link_text"]
         wine_url = wine["wine_url"]
         suffix = wine["suffix"]
-        wine_key = f"{OUTPUT_PREFIX}/{today_date_str}/json/wines/{suffix}.json"
-        print(f"wine_key = {wine_key}")
-        try:
-            read_dict_from_json_s3(bucket=OUTPUT_BUCKET, key=wine_key)
-            count += 1
-            print(f"found wine_key = {wine_key}")
-            row_df = pd.DataFrame(dict(
-                page_key=[page_key],
-                wine_url=[wine_url],
-                link_text=[link_text],
-                suffix=[suffix],
-                wine_key=[wine_key],
-                detect_missing=False
-            ))
-        except:
-            print(f"missing wine_key = {wine_key}")
-            row_df = pd.DataFrame(dict(
-                page_key=[page_key],
-                wine_url=[wine_url],
-                link_text=[link_text],
-                suffix=[suffix],
-                wine_key=[wine_key],
-                detect_missing=True
-            ))
-        dfs_to_concat.append(row_df)
+        wine_key_html = f"{OUTPUT_PREFIX}/{today_date_str}/html/wines/{suffix}.html"
+        has_html = check_for_html(wine_key_html)
+        wine_key_json = f"{OUTPUT_PREFIX}/{today_date_str}/json/wines/{suffix}.json"
+        print(f"wine_key_json = {wine_key_json}")
+        has_json = check_for_json(count, wine_key_json)
+        dfs_to_concat.append(pd.DataFrame(dict(
+            page_key=[page_key],
+            wine_url=[wine_url],
+            link_text=[link_text],
+            suffix=[suffix],
+            wine_key=[wine_key_json],
+            has_html=[has_html],
+            has_json=[has_json]
+        )))
     df = pd.concat(dfs_to_concat)
+    df = df.reset_index(drop=True)
     df.index.name = "index"
     print(f"df.shape = {df.shape}")
     write_csv_to_s3(
