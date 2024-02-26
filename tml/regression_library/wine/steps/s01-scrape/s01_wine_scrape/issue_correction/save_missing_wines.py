88427e312e22101fb2b201c65c23e586dda1253e
wilsonify <tom.andrew.wilson@gmail.com>
2024-02-26 07:27:34 -0500

    save_missing_wines.py

--- tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/issue_correction/save_missing_wines.py
+++ tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/issue_correction/save_missing_wines.py
@@ -12,7 +12,7 @@ from s01_wine_scrape import OUTPUT_BUCKET, OUTPUT_PREFIX
 from s01_wine_scrape.pa_wines.s04_wine_html_to_json import main_scrape_one_wine
 
 
-def main_scrape_missing_wine_one_page(page_key):
+def main_correct_missing_one_page(page_key):
     today_date_str = datetime.now().strftime('%Y-%m-%d')
     s3_session = Session()
     s3_client = s3_session.client('s3')
@@ -55,7 +55,7 @@ def main_scrape_wine_parallel():
         glob_pattern='*_missing.csv'
     )
     with ProcessPoolExecutor(max_workers=8) as executor:
-        executor.map(main_scrape_missing_wine_one_page, objs)
+        executor.map(main_correct_missing_one_page, objs)
 
 
 if __name__ == "__main__":
