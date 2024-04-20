f01fcfc3917d8826beb0b3304d3b58f9940df9d9
wilsonify <tom.andrew.wilson@gmail.com>
2024-04-20 11:21:08 -0400

    enqueue_scrape_one_page_pa in s01_save_pages_to_html.py

--- tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/available.py
+++ tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/available.py
@@ -1,17 +1,28 @@
-from s01_wine_scrape.issue_correction.save_missing_wines import main_correct_missing_one_page
-from s01_wine_scrape.issue_detection.detect_missing import main_detect_missing
-from s01_wine_scrape.pa_wines.create.s01_save_pages_to_html import main_scrape_wine_pa
-from s01_wine_scrape.pa_wines.create.s02_page_html_to_json import main_page_html_to_json
+from s01_wine_scrape.issue_correction.save_missing_wines import main_correct_missing_one_page, enqueue_correct_missing
+from s01_wine_scrape.issue_detection.detect_missing import main_detect_missing, enqueue_detect_missing
+from s01_wine_scrape.pa_wines.create.s01_save_pages_to_html import (
+    main_scrape_wine_pa,
+    scrape_one_page_pa,
+    enqueue_scrape_one_page_pa
+)
+from s01_wine_scrape.pa_wines.create.s02_page_html_to_json import main_page_html_to_json, enqueue_page_html_to_json
 from s01_wine_scrape.pa_wines.create.s03_save_wines_to_html import main_scrape_wine_one_page
 from s01_wine_scrape.pa_wines.create.s04_wine_html_to_json import main_scrape_one_wine
-from s01_wine_scrape.pa_wines.create.s05_wine_json_to_csv import main_json_to_csv
+from s01_wine_scrape.pa_wines.create.s05_wine_json_to_csv import main_json_to_csv, enqueue_json_to_csv
 
 available = {
-    "main_scrape_wine_pa": main_scrape_wine_pa,
+    "enqueue_correct_missing": enqueue_correct_missing,
+    "enqueue_detect_missing": enqueue_detect_missing,
+    "enqueue_json_to_csv": enqueue_json_to_csv,
+    "enqueue_page_html_to_json": enqueue_page_html_to_json,
+    "enqueue_scrape_one_page_pa": enqueue_scrape_one_page_pa,
+    "main_correct_missing_one_page": main_correct_missing_one_page,
+    "main_detect_missing": main_detect_missing,
+    "main_json_to_csv": main_json_to_csv,
     "main_page_html_to_json": main_page_html_to_json,
     "main_scrape_one_wine": main_scrape_one_wine,
     "main_scrape_wine_one_page": main_scrape_wine_one_page,
-    "main_json_to_csv": main_json_to_csv,
-    "main_detect_missing": main_detect_missing,
-    "main_correct_missing_one_page": main_correct_missing_one_page
+    "main_scrape_wine_pa": main_scrape_wine_pa,
+    "scrape_one_page_pa": scrape_one_page_pa,
+
 }
--- tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/pa_wines/create/s01_save_pages_to_html.py
+++ tml/regression_library/wine/steps/s01-scrape/s01_wine_scrape/pa_wines/create/s01_save_pages_to_html.py
@@ -1,7 +1,9 @@
+import json
 import math
 import time
 from datetime import datetime
 
+import boto3
 import bs4
 import requests
 from boto3 import Session
@@ -13,7 +15,22 @@ from s01_wine_scrape import (
 )
 
 
+def scrape_one_page_pa(i):
+    """scrape one html page"""
+    url = f"{baseurl}/{i}"
+    today_date_str = datetime.now().strftime('%Y-%m-%d')
+    s3_session = Session()
+    s3_client = s3_session.client('s3')
+    resp = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=100)
+    s3_client.put_object(
+        Body=resp.text,
+        Bucket=OUTPUT_BUCKET,
+        Key=f"{OUTPUT_PREFIX}/{today_date_str}/html/pages/page_{i:02d}.html"
+    )
+
+
 def main_scrape_wine_pa():
+    """scrape all html pages in series"""
     today_date_str = datetime.now().strftime('%Y-%m-%d')
     s3_session = Session()
     s3_client = s3_session.client('s3')
@@ -29,11 +46,31 @@ def main_scrape_wine_pa():
         print(url)
         print(f"{i}/{total_pages} = {i / total_pages:0.2f}")
         resp = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=100)
-        soup = bs4.BeautifulSoup(resp.text, features="lxml")
         s3_client.put_object(
             Body=resp.text,
             Bucket=OUTPUT_BUCKET,
             Key=f"{OUTPUT_PREFIX}/{today_date_str}/html/pages/page_{i:02d}.html"
+        )
+
+
+def enqueue_scrape_one_page_pa():
+    sqs = boto3.client('sqs')
+    queue_url = 'wine-sqs-try'
+    resp = requests.get(baseurl, headers={"User-Agent": "Mozilla/5.0"}, timeout=100)
+    soup = bs4.BeautifulSoup(resp.text, features="lxml")
+    total_items = soup.find("span", class_="countItems").text.strip(" Items").replace(",", "")
+    total_items = int(total_items)
+    total_pages = math.floor(total_items / 25.0)
+    for i in range(1, total_pages + 1):
+        print(f"{i}/{total_pages} = {i / total_pages:0.2f}")
+        message_dict = {
+            "strategy": "scrape_one_page_pa",
+            "i": i
+        }
+        message_str = json.dumps(message_dict)
+        sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=message_str
         )
 
 
