d8859c7653c3f2b509b3187d3ac376383803aa2c
wilsonify <tom.andrew.wilson@gmail.com>
2025-04-12 11:10:08 -0400

    s02_extract_books.py

---
+++ tml/classification_library/ebooks/s02_extract_books.py
@@ -0,0 +1,71 @@
+import json
+import os
+import sqlite3
+
+import pandas as pd
+
+
+def safe_filename(book_id, title):
+    sanitized_title = (
+        title[:50]
+        .replace('/', '-')
+        .replace('\\', '-')
+        .replace(' ', '-')
+    )
+    return f"{book_id:02d}-{sanitized_title}.json"
+
+
+def export_books_to_json(sqlite_file, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    with sqlite3.connect(sqlite_file) as conn:
+        books_df = pd.read_sql("SELECT * FROM books LIMIT 3", conn)
+        for _, row in books_df.iterrows():
+            book_id = row['id']
+            book_uuid = row['uuid']
+            title = row['title']
+            filename = safe_filename(book_id, title)
+            file_path = os.path.join(output_dir, filename)
+            author_id = pd.read_sql(f"""SELECT author FROM books_authors_link WHERE book = {book_id}""", conn).loc[
+                0, "author"]
+            author_name = pd.read_sql(f"""SELECT name FROM authors WHERE id = {author_id}""", conn).loc[0, "name"]
+            publisher_id = \
+                pd.read_sql(f"""SELECT publisher FROM books_publishers_link WHERE book={book_id}""", conn).loc[
+                    0, "publisher"]
+            publisher_name = pd.read_sql(f"""SELECT name FROM publishers WHERE id = {publisher_id}""", conn).loc[
+                0, "name"]
+
+            try:
+                series_id = (
+                    pd.read_sql(f"""SELECT series FROM books_series_link WHERE book = {book_id}""", conn)
+                    .loc[0, "series"]
+                )
+                series_name = pd.read_sql(f"""SELECT name FROM series WHERE id = {series_id}""", conn).loc[0, "name"]
+            except:
+                series_name = ""
+
+            tags_ids = pd.read_sql(f"""SELECT tag FROM books_tags_link WHERE book={book_id}""", conn)
+            tags_list = tags_ids["tag"].to_list()
+            try:
+                comments_text = pd.read_sql(f"""SELECT text FROM comments WHERE book={book_id}""", conn).loc[0, "text"]
+            except:
+                comments_text = ""
+
+            book_dict = dict(
+                book_id=row['id'],
+                book_uuid=row["uuid"],
+                title=row["title"],
+                pubdate=row["pubdate"],
+                isbn=row["isbn"],
+                author_name=author_name,
+                publisher_name=publisher_name,
+                series_name=series_name,
+                series_index=row["series_index"],
+                tags=tags_list,
+                description=comments_text,
+            )
+            json.dump(book_dict, open(file_path, 'w'), default=str, indent=2)
+
+
+# Example usage
+if __name__ == "__main__":
+    export_books_to_json("data/input/metadata.db", "data/output_actual/books_metadata")
